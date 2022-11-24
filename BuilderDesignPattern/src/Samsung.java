c8f2fabc5943dc931ba2e63c9e5aa0def55f6f12
DhananjayBobde <113025710+DhananjayBobde@users.noreply.github.com>
2022-11-24 14:03:05 +0530

    Builder Pattern

---
+++ BuilderDesignPattern/src/Samsung.java
@@ -0,0 +1,11 @@
+public class Samsung extends Company{
+    @Override
+    public String pack() {
+        return "Samsung CD";
+    }
+
+    @Override
+    public int price() {
+        return 15;
+    }
+}
